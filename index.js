5bcdacf3ec041a2616772355bd2f7814c88a08f9
AdrianDiazCode <eiaddiaz@utp.edu.co>
2023-02-17 03:52:21 -0500

    test

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const exec = require("child_process").exec;
 
 const stackName = core.getInput("StackName");
 const outputsString = core.getInput("Outputs");
-const outputs = outputsString.split(",").map((str) => str.trim());
+const outputNames = outputsString.split(",").map(function (str) {
+  return str.trim();
+});
 
 core.setOutput("TestOutput", "TestValue");
 
@@ -11,13 +13,23 @@ const getOutputScript = function (desiredOutput) {
   return `aws cloudformation describe-stacks  --query "Stacks[?StackName=='${stackName}'][].Outputs[?OutputKey=='${desiredOutput}'].OutputValue" --output text`;
 };
 
-for (const output of outputs) {
-  console.log("output:", output);
-  const outputScript = getOutputScript(output);
+const getOutput = function (outputName) {
+  const outputScript = getOutputScript(outputName);
   console.log("outputScript:", outputScript);
-
-  exec(outputScript, function (_, outputScriptResult, _) {
-    console.log("outputScriptResult:", outputScriptResult);
-    core.setOutput(output, outputScriptResult);
+  return new Promise(function (resolve) {
+    exec(outputScript, function (_, outputScriptResult) {
+      console.log("outputScriptResult:", outputScriptResult);
+      resolve(outputScriptResult);
+    });
   });
+};
+
+async function main() {
+  for (const desiredOutput of outputNames) {
+    console.log("output:", desiredOutput);
+    const result = await getOutput(desiredOutput);
+    core.setOutput(desiredOutput, result);
+  }
 }
+
+main();
